8a302ec97725238adcbac0fabd33a386ff47e21d
moazmohamed20 <moazmohamed15@outlook.com>
2022-01-21 16:01:57 +0200

    Selection Sort

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -7,6 +7,12 @@ private:
 	int size;
 	int *mArray;
 
+	void swap(int a, int b) {
+		int temp = mArray[a];
+		mArray[a] = mArray[b];
+		mArray[b] = temp;
+	}
+
 public:
 	CustomArray(int initialSize = 100) {
 		size = 0;
@@ -19,9 +25,38 @@ public:
 
 	int getSize() { return size; }
 
+	void selectionSort() {
+		for (int i = 0; i < size; i++) {
+
+			// Find Minimum Value After i
+			int minPos = i;
+			for (int j = i; j < size; j++)
+				if (mArray[j] < mArray[minPos])
+					minPos = j;
+
+			// Swap Current Index With Minimum Value
+			swap(i, minPos);
+		}
+	}
+
 };
 
 int main() {
+	CustomArray customArray;
+
+	// Generating 10 Random Numbers
+	for (int i = 0; i < 10; i++)
+		customArray.insert(rand() % 100);
+
+	cout << "Random Array Before Sorting:\n";
+	for (int i = 0; i < 10; i++)
+		cout << customArray.get(i) << "\t";
+
+	customArray.selectionSort();
+
+	cout << "\n\nRandom Array After Sorting:\n";
+	for (int i = 0; i < 10; i++)
+		cout << customArray.get(i) << "\t";
 
 
 	getchar();
