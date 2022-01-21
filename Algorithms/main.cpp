264ea134a9827dcc273f8853b9978154b3091c76
moazmohamed20 <moazmohamed15@outlook.com>
2022-01-21 15:44:38 +0200

    CustomArray Class Created

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+class CustomArray {
+private:
+	int size;
+	int *mArray;
+
+public:
+	CustomArray(int initialSize = 100) {
+		size = 0;
+		mArray = new int[initialSize];
+	}
+
+	void insert(int value) { mArray[size++] = value; }
+
+	int get(int index) { return mArray[index]; }
+
+	int getSize() { return size; }
+
+};
+
 int main() {
-	
+
 
 	getchar();
 	return 0;
